2aecdcb54c0fbc7e34ce8edccca8fda679188ef5
周健力 <m18827526822@163.com>
2021-01-09 17:59:10 +0800

    新增:Main页面Item布局

--- app/src/main/java/com/rz/smart/ui/adapter/GoodsAdapter.kt
+++ app/src/main/java/com/rz/smart/ui/adapter/GoodsAdapter.kt
@@ -13,9 +13,19 @@ class GoodsAdapter() : BaseQuickAdapter<CuisineInfo, BaseViewHolder>(
         helper: BaseViewHolder,
         item: CuisineInfo
     ) {
-        helper.setText(R.id.menuName, item.GoodsName)
-        helper.setText(R.id.menuPrice, "单价:${item.CostPrice}")
-        helper.setText(R.id.menuWeight, "库存:${item.GoodsCode}")
+//        helper.setText(R.id.menuName, item.GoodsName)
+//        helper.setText(R.id.menuPrice, "单价:${item.CostPrice}")
+//        helper.setText(R.id.menuWeight, "库存:${item.GoodsCode}")
+        helper.setText(R.id.tvGoodsName,item.GoodsName)
+        helper.setText(R.id.tvSupplierName,"供应商:${item.SupplierName}")
+        helper.setText(R.id.tvGoodsTypeName,"分类:${item.GoodsTypeName}")
+
+        helper.setText(R.id.tvCostPrice,"单价:${item.CostPrice}")
+        helper.setText(R.id.tvGoodsStock,"库存:${item.GoodsStock}")
+        helper.setText(R.id.tvGoodsCode,"编号:${item.GoodsCode}")
+
+
+
     }
 
 }
