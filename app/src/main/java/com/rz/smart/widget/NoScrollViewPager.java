aa844e020fa8a3d72d6a8e8807ec0a4d88d0b0a6
周健力 <m18827526822@163.com>
2021-01-09 13:03:22 +0800

    修复:loginActivity viewpager禁止滑动

--- app/src/main/java/com/rz/smart/widget/NoScrollViewPager.java
+++ app/src/main/java/com/rz/smart/widget/NoScrollViewPager.java
@@ -12,54 +12,61 @@ import androidx.viewpager.widget.ViewPager;
  * 禁止滑动的viewpager
  */
 public class NoScrollViewPager extends ViewPager {
-    private boolean isScroll;
+
+    private boolean noScroll = true;
+    private boolean noSmoothScroll = false;
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
     public NoScrollViewPager(Context context) {
         super(context);
     }
-    /**
-     * 1.dispatchTouchEvent一般情况不做处理
-     *,如果修改了默认的返回值,子孩子都无法收到事件
-     */
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        return super.dispatchTouchEvent(ev);   // return true;不行
+
+    public void setNoScroll(boolean noScroll) {
+        this.noScroll = noScroll;
     }
-    /**
-     * 是否拦截
-     * 拦截:会走到自己的onTouchEvent方法里面来
-     * 不拦截:事件传递给子孩子
-     */
+
+    public void setNoSmoothScroll(boolean noSmoothScroll) {
+        this.noSmoothScroll = noSmoothScroll;
+    }
+
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        // return false;//可行,不拦截事件,
-        // return true;//不行,孩子无法处理事件
-        //return super.onInterceptTouchEvent(ev);//不行,会有细微移动
-        if (isScroll){
-            return super.onInterceptTouchEvent(ev);
-        }else{
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        if (noScroll) {
             return false;
+        } else {
+            return super.onTouchEvent(arg0);
         }
     }
-    /**
-     * 是否消费事件
-     * 消费:事件就结束
-     * 不消费:往父控件传
-     */
+
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        //return false;// 可行,不消费,传给父控件
-        //return true;// 可行,消费,拦截事件
-        //super.onTouchEvent(ev); //不行,
-        //虽然onInterceptTouchEvent中拦截了,
-        //但是如果viewpage里面子控件不是viewgroup,还是会调用这个方法.
-        if (isScroll){
-            return super.onTouchEvent(ev);
-        }else {
-            return true;// 可行,消费,拦截事件
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        if (noScroll) {
+            return false;
+        } else {
+            return super.onInterceptTouchEvent(arg0);
         }
     }
-    public void setScroll(boolean scroll) {
-        isScroll = scroll;
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        if (noSmoothScroll == false) {
+            super.setCurrentItem(item, smoothScroll);
+        }else{
+            super.setCurrentItem(item,noSmoothScroll);
+        }
+
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        super.setCurrentItem(item);
     }
 }
